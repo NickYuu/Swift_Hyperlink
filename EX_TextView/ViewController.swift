4694dddeb30a1c6761877cad0d32ddeba472567d
NickYuu <s3310304067@gms.nutc.edu.tw>
2017-10-16 13:08:40 +0800

    Initial Commit

---
+++ EX_TextView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EX_TextView
+//
+//  Created by TsungHan on 2017/10/16.
+//  Copyright © 2017年 TsungHan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
